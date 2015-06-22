10ff4ac611eafec9bb6c8c4264f6568d2c8e3b63
雷翊廷 <yiting@herxun.co>
2015-06-22 17:20:53 +0800

    add user info

---
+++ Joka/JKEnterUserInfoViewController.h
@@ -0,0 +1,17 @@
+//
+//  JKEnterUserInfoViewController.h
+//  Joka
+//
+//  Created by 雷翊廷 on 2015/6/22.
+//  Copyright (c) 2015年 雷翊廷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKEnterUserInfoViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *experienceText;
+@property (weak, nonatomic) IBOutlet UITextField *levelText;
+@property (weak, nonatomic) IBOutlet UITextField *awardText;
+@property (weak, nonatomic) IBOutlet UITextField *locationText;
+
+@end
