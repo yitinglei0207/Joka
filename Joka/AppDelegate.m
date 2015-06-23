5d0f0d553ef42a14c0f73c689dc2015d8eb89e1f
雷翊廷 <yiting@herxun.co>
2015-06-23 17:02:35 +0800

    add basic push

--- Joka/AppDelegate.m
+++ Joka/AppDelegate.m
@@ -10,7 +10,8 @@
 #import "AnPush.h"
 #import "JKPushDelegate.h"
 #import "JokaCredentials.h"
-@interface AppDelegate ()
+#import "JKLightspeedManager.h"
+@interface AppDelegate ()<AnPushDelegate>
 
 @end
 
@@ -19,21 +20,103 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    [AnPush registerForPushNotification:(UIRemoteNotificationTypeAlert|
-                                         UIRemoteNotificationTypeBadge|
-                                         UIRemoteNotificationTypeSound)];
-    NSData *tokenData = [[NSUserDefaults standardUserDefaults] objectForKey:@"deviceToken"];
-    if (tokenData)
-        [AnPush setup:LIGHTSPEED_APP_KEY deviceToken:tokenData delegate:self secure:YES];
+    
+//    NSData *tokenData = [[NSUserDefaults standardUserDefaults] objectForKey:@"deviceToken"];
+//    
+//    if (tokenData)
+//        [AnPush setup:LIGHTSPEED_APP_KEY deviceToken:tokenData delegate:self secure:YES];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(registerPushNotification)
+                                                 name:@"connect"
+                                               object:nil];
+    
     return YES;
 }
 
-//- (void) registerPushNotification
+- (void) registerPushNotification
+{
+    [AnPush registerForPushNotification:(UIUserNotificationTypeAlert|
+                                         UIUserNotificationTypeBadge|
+                                         UIUserNotificationTypeSound)];
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    NSLog(@"error: %@", error);
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    [AnPush setup:LIGHTSPEED_APP_KEY deviceToken:deviceToken delegate:self secure:YES];
+    [[AnPush shared] register:@[ @"Joka_Push" ] overwrite:YES];
+    //    NSArray *channels = [NSArray arrayWithObjects:@"BroadcastMessage", @"Payroll", @"ImportantNews"];
+    //    [[AnPush shared] register:channels overwrite:YES];
+    //    
+}
+
+
+#pragma mark - AnPushDelegate functions
+- (void)didRegistered:(NSString *)anid withError:(NSString *)error
+{
+    NSLog(@"Arrownock didRegistered\nError: %@", error);
+    if (error && ![error isEqualToString:@""])
+    {
+        NSLog(@"LSIM AppDelegate, AnPush failed to register, error: %@", error);
+    }
+    else if (!anid || [anid isEqualToString:@""])
+    {
+        NSLog(@"LSIM AppDelegate, AnPush failed to register, invalid Lightspeed ID");
+    }
+    else
+    {
+        /* use the anId to bind AnIM & AnPush */
+        [[[JKLightspeedManager manager]anIM] bindAnPushService:anid appKey:LIGHTSPEED_APP_KEY deviceType:AnPushTypeiOS];
+    }
+}
+
+- (void)didUnregistered:(BOOL)success withError:(NSString *)error
+{
+    NSLog(@"Unregistration success: %@\nError: %@", success? @"YES" : @"NO", error);
+    if (!success)
+    {
+        /* do nothing */
+    }
+}
+
+//- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
 //{
-//    [AnPush registerForPushNotification:(UIRemoteNotificationTypeAlert|
-//                                         UIRemoteNotificationTypeBadge|
-//                                         UIRemoteNotificationTypeSound)];
+//    // only perform the action associated with this noitification if we're being brought to the foreground
+//    // by the user swiping the notification or otherwise triggering it's action.
+//    
+//    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
+//    
+//    if (application.applicationState == UIApplicationStateActive)
+//    {
+//        
+//    }else if (application.applicationState == UIApplicationStateInactive)
+//    {
+//        NSLog(@"Notification revieced!!! ================\n%@", userInfo);
+//        HXTabBarViewController *vc = (HXTabBarViewController*) self.window.rootViewController;
+//        [HXIMManager manager].remoteNotificationInfo = [userInfo mutableCopy];
+//        
+//        [self resetTabBarViews];
+//        if (vc.selectedIndex != 0) {
+//            vc.selectedIndex = 0;
+//        }
+//    }
 //}
+//
+//- (void)resetTabBarViews {
+//    HXTabBarViewController *tabBarController = (HXTabBarViewController *)self.window.rootViewController;
+//    for(UIViewController *foo in tabBarController.viewControllers) {
+//        if([foo isKindOfClass:[UINavigationController class]]) {
+//            UINavigationController *bar = (UINavigationController*)foo;
+//            [bar popToRootViewControllerAnimated:NO];
+//        }
+//    }
+//}
+
 
 
 - (void)applicationWillResignActive:(UIApplication *)application {
@@ -58,12 +141,7 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-{
-    [AnPush setup:deviceToken deviceToken:deviceToken delegate:self secure:YES];
-//    NSArray *channels = [NSArray arrayWithObjects:@"BroadcastMessage", @"Payroll", @"ImportantNews"];
-//    [[AnPush shared] register:channels overwrite:YES];
-}
+
 
 
 
---
+++ Joka/JKTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  JKTabBarViewController.h
+//  Joka
+//
+//  Created by 雷翊廷 on 2015/6/23.
+//  Copyright (c) 2015年 雷翊廷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKTabBarViewController : UITabBarController
+
+@end
