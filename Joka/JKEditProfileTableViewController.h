4ea5e20ce4e4ee3d7e193e45ee7708365181bdcd
雷翊廷 <yiting@herxun.co>
2015-06-25 17:42:19 +0800

    edit MemberCard and fixed Wall

---
+++ Joka/JKEditProfileTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JKEditProfileTableViewController.h
+//  Joka
+//
+//  Created by 雷翊廷 on 2015/6/25.
+//  Copyright (c) 2015年 雷翊廷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKEditProfileTableViewController : UITableViewController
+
+@end
